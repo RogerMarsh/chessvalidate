36f05093f929df5db2158d8eded1c3d01ddf98f8
Roger Marsh <roger.marsh@solentware.co.uk>
2022-08-22 08:20:51 +0000

    Add EventDetails class to customise Header class from ecfformat project.
    The EventDetails editor is used to supply the formal event identity in
    preference to including it in the results document.
    
    Change Configuration class to customise Configuration class from
    solentware_misc project.

--- chessvalidate/gui/eventdetails.py
+++ chessvalidate/gui/eventdetails.py
@@ -4,6 +4,9 @@
 
 """Set and modify event details."""
 from ecfformat.gui import header
+import ecfformat.core.constants
+
+from ..core import configuration
 
 
 class EventDetails(header.Header):
@@ -14,3 +17,44 @@ class EventDetails(header.Header):
     submission file.
 
     """
+    _sequences = ()
+    _allowed_inserts = {}
+    _popup_menu_label_map = {}
+    _NEW_FILE_TEXT = "\n".join(
+        (
+            "#EVENT DETAILS",
+            "#EVENT CODE=",
+            "#SUBMISSION INDEX=",
+            "#EVENT NAME=",
+            "#EVENT DATE=",
+            "#FINAL RESULT DATE=",
+            "#RESULTS OFFICER=",
+            "#RESULTS OFFICER ADDRESS=",
+            "#TREASURER=",
+            "#TREASURER ADDRESS=",
+        )
+    )
+    _NO_VALUE_TAGS = (
+        ecfformat.core.constants.EVENT_CODE,
+        ecfformat.core.constants.SUBMISSION_INDEX,
+    )
+
+    def _set_field_delete_binding(self):
+        """Suppress Alt-KeyPress-Delete binding in all circumstances.
+
+        This customisation of Header presents an immutable form where only
+        the values entered can be changed.
+
+        """
+
+    def _make_configuration(self):
+        """Return a configuration.Configuration instance."""
+        return configuration.Configuration()
+
+    def _show_value_boundary(self, conf):
+        """Return True if configuration file SHOW_VALUE_BOUNDARY is true."""
+        return bool(
+            conf.get_configuration_value(
+                ecfformat.core.constants.SHOW_VALUE_BOUNDARY
+            ) == ecfformat.core.constants.SHOW_VALUE_BOUNDARY_TRUE
+        )
